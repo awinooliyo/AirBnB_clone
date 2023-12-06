4bd48b5e55e6b419532e53555b65a1bbe28517dd
Erick Awino <erick.otieno89@gmail.com>
2023-12-06 17:22:46 +0300

    models and test packages

--- models/base_model.py
+++ models/base_model.py
@@ -12,12 +12,12 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """
         Constructor for the base class
-        
+
         args:
             *args: none
             **kwargs: keyword arguments
         """
-        #self.id = str(uuid.uuid4())
+        # self.id = str(uuid.uuid4())
 
         if len(kwargs) > 0:
             for k, v in kwargs.items():
@@ -30,7 +30,6 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
 
-
     def __str__(self):
         """Returns a string"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
@@ -42,10 +41,10 @@ class BaseModel:
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """Returns a dictionary containing 
+        """Returns a dictionary containing
         key/values of __dict__ of the instance
         """
-        bnb_dictionary = self.__dict__.copy() #using the shallowcopy function
+        bnb_dictionary = self.__dict__.copy()
         bnb_dictionary['__class__'] = self.__class__.__name__
         bnb_dictionary['created_at'] = self.created_at.isoformat()
         bnb_dictionary['updated_at'] = self.updated_at.isoformat()
