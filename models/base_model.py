2dd185fa70bb93df14408f23755908c366ddd462
Erick Awino <erick.otieno89@gmail.com>
2023-12-06 11:59:41 +0300

    models and test packages

---
+++ models/base_model.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Base Model"""
+
+from datetime import datetime
+import uuid
+
+
+class BaseModel:
+    """Base model that defines all common
+    attributes/methods for other classes
+    """
+    def __init__(self):
+        """Constructor for the base class"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Returns a string"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """Updates yje public instance attribute
+        with the current datetime
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary containing 
+        key/values of __dict__ of the instance
+        """
+        bnb_dictionary = self.__dict__.copy() #using the shallowcopy function
+        bnb_dictionary['__class__'] = self.__class__.__name__
+        bnb_dictionary['created_at'] = self.created_at.isoformat()
+        bnb_dictionary['updated_at'] = self.updated_at.isoformat()
+        return bnb_dictionary
