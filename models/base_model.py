2d21514f6511c301c03c67b79b40e992a20f93bd
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2023-12-08 10:37:00 +0300

    question 5

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,8 @@
-#!/usr/bin/python3
 """Base Model"""
 
 from datetime import datetime
 import uuid
-from models.__init__ import storage
+import models
 
 
 class BaseModel:
@@ -30,7 +29,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         """Returns a string"""
@@ -41,7 +40,7 @@ class BaseModel:
         with the current datetime
         """
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """Returns a dictionary containing
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,36 +1,50 @@
-#!/usr/bin/python2
+"""file_storage module"""
+
 import json
-# from models.base_model import BaseModel
+from models.base_model import BaseModel
+
 
 class FileStorage:
+    """ serializes instances to a JSON file and deserializes
+    JSON file to instances
+    """
+
     __file_path = "file.json"
     __objects = {}
 
-    
     def all(self):
+        """returns the dictionary __objects"""
         return self.__objects
 
-
     def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id"""
         key = f"{obj.__class__.__name__}.{obj.id}"
         self.__objects[key] = obj
+        print(f"obj: {obj}")
 
     def save(self):
+        """serializes __objects to the JSON file (path: __file_path)"""
         try:
             my_dictionary = {}
 
-            for k, v in self.__objects.items():
-                my_dictionary[k] = 
+            for key, value in self.__objects.items():
+                my_dictionary[key] = value.to_dict()
 
             with open(self.__file_path, "w") as file:
-                json.dump(my_dictionaryeModel, file)
+                json.dump(my_dictionary, file)
         except FileNotFoundError:
             pass
 
     def reload(self):
+        """deserializes the JSON file to __objects"""
         try:
             with open(self.__file_path, "r") as file:
-                content = json.load(file)
+                data = json.load(file)
+                for key, value in data.items():
+                    class_name, obj_id = key.split('.')
+                    class_obj = globals().get(class_name)
+                    if class_obj:
+                        obj = class_obj(**value)
+                        self.__objects[key] = obj
         except FileNotFoundError:
             pass
-
